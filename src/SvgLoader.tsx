d4fab60bfd16ba1f6a2ab10a8a7e3e0b8789c50b
Stefano Ruth <stefano6262@gmail.com>
2020-02-14 08:21:27 +0100

    Initial

---
+++ src/SvgLoader.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const SvgLoader: React.FunctionComponent<{
+	url: string
+	className?: string
+}> = props => {
+	const [svg, setSvg] = React.useState('')
+
+	React.useEffect(() => {
+		const abortController = new AbortController()
+		const signal = abortController.signal
+
+		fetch(props.url, { signal })
+			.then(res => res.text())
+			.then(value => {
+				setSvg(value)
+			})
+			.catch(err => {
+				if (err.name !== 'AbortError') {
+					throw err
+				}
+			})
+
+		return () => {
+			abortController.abort()
+		}
+	}, [])
+
+	return <i className={props.className} dangerouslySetInnerHTML={{ __html: svg }} />
+}
