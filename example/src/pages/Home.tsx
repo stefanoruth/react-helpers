e85aa48836667cea9522667fc6f563dbbcbba234
Stefano Ruth <stefanoruth@users.noreply.github.com>
2020-03-13 10:02:25 +0100

    wip

--- example/src/pages/Home.tsx
+++ example/src/pages/Home.tsx
@@ -6,32 +6,34 @@ const linkClass = 'p-4 rounded-lg text-white bg-blue-400 hover:bg-blue-500 flex 
 
 export const HomePage: React.FunctionComponent = props => {
 	return (
-		<div className="p-8">
-			<h1 className="text-3xl">React Components</h1>
+		<div className="p-8 min-h-screen flex justify-center items-center">
 			<div>
-				<Grid className="flex flex-wrap -mx-4" itemClass="">
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-					<Link className={linkClass} to="#">
-						Grid
-					</Link>
-				</Grid>
+				<h1 className="text-3xl">React Components</h1>
+				<div>
+					<Grid className="flex flex-wrap -mx-4" itemClass="w-full sm:w-1/4 p-4">
+						<Link className={linkClass} to="/grid">
+							Grid
+						</Link>
+						<Link className={linkClass} to="/about">
+							About
+						</Link>
+						<Link className={linkClass} to="/modal">
+							Modal
+						</Link>
+						<Link className={linkClass} to="#">
+							Grid
+						</Link>
+						<Link className={linkClass} to="#">
+							Grid
+						</Link>
+						<Link className={linkClass} to="#">
+							Grid
+						</Link>
+						<Link className={linkClass} to="#">
+							Grid
+						</Link>
+					</Grid>
+				</div>
 			</div>
 		</div>
 	)
